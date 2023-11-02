d2368340436924d4ee7abdae45628022d7f0c3d7
Dave Augustus <davetweetlive@gmail.com>
2023-11-03 01:35:39 +0530

    Completed Register API
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- main.go
+++ main.go
@@ -54,17 +54,38 @@ func registerUser(c *gin.Context) {
 	}
 
 	// Connect to the database
-	_, err = connectToDatabase()
+	sql, err := connectToDatabase()
 	if err != nil {
 		c.JSON(500, gin.H{"error": "database error"})
 		return
 	}
 
 	// Check if the email already exist in the db
+	dbUser := User{}
+	err = sql.QueryRow("SELECT first_name,last_name FROM users where email=$1", user.Email).Scan(&dbUser.FirstName, &dbUser.LastName)
+	if err == nil {
+		log.Printf("email already exist")
+		c.JSON(500, gin.H{"error": "email already exist"})
+		return
+	}
 
 	// Check if the username already exist in the db
+	err = sql.QueryRow("SELECT first_name,last_name FROM users where username=$1", user.UserName).Scan(&dbUser.FirstName, &dbUser.LastName)
+	if err == nil {
+		log.Printf("username already exist")
+		c.JSON(500, gin.H{"error": "username already exist"})
+		return
+	}
 
 	// Store the data into the database
+	_, err = sql.Query("INSERT INTO users (first_name,last_name, email, username, password) VALUES($1,$2,$3,$4,$5)", user.FirstName, user.LastName, user.Email, user.UserName, user.Password)
+	if err != nil {
+		log.Printf("cannot insert the value to the database: %+v", err)
+		c.JSON(500, gin.H{"error": "failed to register"})
+		return
+	}
+
+	c.JSON(200, gin.H{"error": "successfully registered"})
 }
 
 func ping(c *gin.Context) {
